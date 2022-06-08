8d4b4da857123f73be00fc088a046095fbf7d9d6
Dmitry Votchitsev <dmitry.votchitsev@gmail.com>
2022-06-08 22:17:15 +0200

    test and lint validation pass

--- src/js/__tests__/test.js
+++ src/js/__tests__/test.js
@@ -1,5 +1,33 @@
-import Hello from '../task';
+import getSpecialProperty from '../task';
 
-test('test', () => {
-  expect(Hello()).toBe('Hello World!');
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      // <- обратите внимание, описание "засекречено"
+    },
+  ],
+};
+
+const expectedResult = [
+  [8, 'Двойной выстрел', 'http://...', 'Двойной выстрел наносит двойной урон'],
+  [9, 'Нокаутирующий удар', 'http://...', 'Описание не доступно'],
+];
+
+test('test getSpecialProperty', () => {
+  expect(getSpecialProperty(character)).toStrictEqual(expectedResult);
 });
--- src/js/task.js
+++ src/js/task.js
@@ -1,3 +1,11 @@
-export default function Hello() {
-  return 'Hello World!';
+export default function getSpecialProperty(character) {
+  const result = [];
+
+  character.special.forEach((attack) => {
+    const {
+      id, name, icon, description = 'Описание не доступно',
+    } = attack;
+    result.push([id, name, icon, description]);
+  });
+  return result;
 }
